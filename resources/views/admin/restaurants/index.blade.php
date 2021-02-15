03db2baad2b8e4e664c0871d905a6c976a327615
Ivan <delfatti.ivan@gmail.com>
2021-02-15 18:21:01 +0100

    Add buttons index

--- resources/views/admin/restaurants/index.blade.php
+++ resources/views/admin/restaurants/index.blade.php
@@ -8,9 +8,28 @@
         @endif
 
         <ul>
-            @foreach ($restaurants as $restaurant)
-                <li>{{$restaurant->name}}</li>
-            @endforeach
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Created</th>
+                            <th colspan=“3”></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($restaurants as $restaurant)
+                            <tr>
+                                <td>{{$restaurant->id}}</td>
+                                <td>{{$restaurant->name}}</td>
+                                <td>{{$restaurant->created_at->format('d/m/Y')}}</td>
+                                <td> <a class="btn btn-success" href="">Mostra</a></td>
+                                <td> <a class="btn btn-primary" href="{{ route('admin.restaurants.edit', $restaurant->id) }}">Modifica</a></td>
+                                <td> <a class="btn btn-danger" href="">Elimina</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
         </ul>
     </div>
 

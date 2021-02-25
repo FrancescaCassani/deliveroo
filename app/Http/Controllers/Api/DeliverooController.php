965332d584e1600878724867536fb049c32a45e6
Ivan <delfatti.ivan@gmail.com>
2021-02-25 16:04:40 +0100

    Fix tutti i generi

--- app/Http/Controllers/Api/DeliverooController.php
+++ app/Http/Controllers/Api/DeliverooController.php
@@ -13,10 +13,10 @@ class DeliverooController extends Controller
 {
     public function index() {
 
-        $allRestaurants = [];
-        
+        $allRestaurants = Restaurant::all();
         $genres = Genre::all();
         if (!empty($_GET['genre'])) {
+            $allRestaurants = [];
             $searchGenre = $_GET['genre'];
             foreach ($searchGenre as $genre) {
                 $restaurants = DB::table('genre_restaurant')
@@ -24,17 +24,12 @@ class DeliverooController extends Controller
                 ->join('genres', 'genres.id', '=', 'genre_restaurant.genre_id')
                 ->where('genres.type', '=', $genre)
                 ->get();
-
+                
                 $allRestaurants = $restaurants;
             }
-        } else {
-            $allRestaurants = DB::table('genre_restaurant')
-            ->join('restaurants', 'restaurants.id', '=', 'genre_restaurant.restaurant_id')
-            ->join('genres', 'genres.id', '=', 'genre_restaurant.genre_id')
-            ->get();
         }
-
         return response()->json($allRestaurants);
+
     }
 
     public function food() {

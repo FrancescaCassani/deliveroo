0158e4fb2da3ca78171252d941f883c699e5ca91
Francesca <fracassani@gmail.com>
2021-03-03 16:04:31 +0100

    Show ristoranti guest - Franci

--- resources/views/guests/restaurants/show.blade.php
+++ resources/views/guests/restaurants/show.blade.php
@@ -11,26 +11,67 @@
 <div class="hero-restaurant">
     <div class="hero-info">
         <h2>{{$restaurant->name}}</h2>
-        <p class="lead mt-5">{{$restaurant->description}}</p>
+
+        @foreach ($restaurant->genres as $genre)
+            <p>{{$genre->type}}</p>
+        @endforeach
+
+        <p class="lead mt-5">{{$restaurant->address}}</p>
+        <p class="lead">{{$restaurant->description}}</p>
     </div>
+
     <div class="hero-img">
         <img src="{{asset('storage/' . $restaurant->path_img)}}" alt="{{$restaurant->name}}">
     </div>
 </div>
 
-<div class="foods container">
-    <ul class="food-list">
-        @foreach ($restaurant->foods as $food)
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">{{$food->name}}</h5>
-                    <p class="card-text">{{$food->ingredients}}</p>
-                    <a href="#" @click="addCart({{$food}})">Aggiungi al carrello</a>
+
+
+<section class="guest-bg">
+    <div class="container">
+        <div class="box-food">
+            @foreach ($restaurant->foods as $food)
+                <div class="box-detail mr-5 ">
+                    <div class="text">
+                        <h5 >{{$food->name}}</h5>
+                        <p >{{$food->ingredients}}</p>
+                        <p >{{$food->price}} €</p>
+                        <a class="btn btn-primary" href="#" @click="addCart({{$food}})">Aggiungi al carrello</a>
+                    </div>
+
+                    <div class="image">
+                        <img class="mb-2 mt-2" width="160" height="170" src="{{asset('storage/' . $food->path_img)}}" alt="{{$food->name}}">
+                    </div>
                 </div>
-            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+{{-- <div class="container">
+    <div class="row">
+        @foreach ($restaurant->foods as $food)
+        <div class="col-sm-12 col-md-6 col-lg-4 text-center">
+            <ul class="mt-4 list-unstyled list-group-item">
+                <img class="mb-2 mt-2" width="160" height="170" src="{{asset('storage/' . $food->path_img)}}" alt="{{$food->name}}">
+                <h5 class="card-text">{{$food->name}}</h5>
+                <p class="card-text">{{$food->ingredients}}</p>
+                <p class="card-text">{{$food->price}} €</p>
+                <a href="#" @click="addCart({{$food}})">Aggiungi al carrello</a>
+            </ul>
         </div>
         @endforeach
-    </ul>
-</div>
+    </div>
+</div> --}}
+
 @endsection

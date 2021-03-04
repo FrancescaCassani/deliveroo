caaffdddc3324e26cd4bb787dbbe74576154ad41
Francesco <francescomartinmail@gmail.com>
2021-03-04 19:15:35 +0100

    admin fixed true

--- resources/views/partials/header-admin.blade.php
+++ resources/views/partials/header-admin.blade.php
@@ -5,11 +5,17 @@
         </a>
 
         <div class="navbar-nav">
-            <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-            </a>
+           
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                 document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+
         </div>
     </div>
 </nav>
